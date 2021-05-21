d88d8e175df2c6a364c69cc1dcd07e9b7b4ef08e
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-20 23:49:12 -0300

    Inserido trecho do código contendo a piada que havia sido removido do arquivo HTML anteriormente

--- template-node-fulljs/src/views/pages/home/Home.js
+++ template-node-fulljs/src/views/pages/home/Home.js
@@ -1,7 +1,7 @@
-import {api, postform} from "../../../service/api";
+import { api, postform } from "../../../service/api";
 
 async function getRandonJoke() {
-  const request = await api.get('random');
+  const request = await api.get("random");
   const response = request.data;
   return response;
 }
@@ -10,15 +10,16 @@ let Home = {
   is_private: false,
 
   render: async () => {
-    const jokes = await getRandonJoke()
-      let view = /*html*/`
+    const jokes = await getRandonJoke();
+    let view = /*html*/ `
           <div>
             <h1>Piadas aleatórias de Chuck Norris</h1>
             <div class="container">
               <img id="image" src=${jokes.icon_url}>
             </div>
-            </div>
-            <div>
+            <p>${jokes.value}</p>
+          </div>
+          <div>
             <h2>Formulário usando Webhook</h2>
             <p>O mesmo é usado apenas para efeito de demostração<p>
             <p>Quando clicado em enviar, salva as informações no Webhook<p>
@@ -31,31 +32,32 @@ let Home = {
           </div>
       `;
 
-      return view
+    return view;
   },
 
   after_render: async () => {
-    let formcontent = document.getElementById('form');
+    let formcontent = document.getElementById("form");
 
-    formcontent.addEventListener('submit', (e) => {
+    formcontent.addEventListener("submit", (e) => {
       e.preventDefault();
-      let name = document.querySelector('#name').value;
-      let email = document.querySelector('#email').value;
-      let phone = document.getElementById('phone').value;
+      let name = document.querySelector("#name").value;
+      let email = document.querySelector("#email").value;
+      let phone = document.getElementById("phone").value;
 
       let postData = {
         name,
         email,
-        phone
-      }
+        phone,
+      };
 
-      postform.post('', postData).then(
-        response => {
-          alert('Tudo Certo')
-        }
-      ). catch(e => alert('Algo de errado não está certo...'))
+      postform
+        .post("", postData)
+        .then((response) => {
+          alert("Tudo Certo");
+        })
+        .catch((e) => alert("Algo de errado não está certo..."));
     });
-  }
-}
+  },
+};
 
 export default Home;
