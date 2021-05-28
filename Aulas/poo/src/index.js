0979aad6e3cbe2a35bbd0ca333b73ac5ba002dc5
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-28 17:34:30 -0300

    Adicionado arquivo usado na aula Getters and Setters

--- Aulas/poo/src/index.js
+++ Aulas/poo/src/index.js
@@ -1,9 +1,12 @@
 const objJavascript = require('./literals/objeto');
 const ObjJSON = require('./literals/objeto.json');
 const Pessoa = require('./class//anatomia');
+const mariane = require('./class/gettersAndSetters');
 
-console.log(objJavascript.getFinalPrice());
+// console.log(objJavascript.getFinalPrice());
 
-console.log(ObjJSON.produto[0]);
+// console.log(ObjJSON.produto[0]);
 
-console.log(Pessoa.hello('Ailson', 'dev', 38));
+// console.log(Pessoa.hello('Ailson', 'dev', 38));
+
+console.log(mariane.name);
