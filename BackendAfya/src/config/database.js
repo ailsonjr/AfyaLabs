4b4b9c90c0f9d49ebad97a1b86306714709010ce
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-29 16:50:44 -0300

    Desenvolvido arquivo backend do 0

---
+++ BackendAfya/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'tuffi.db.elephantsql.com',
+  username: 'zcejjhgh',
+  password: 'A1paddFsjk6wgdW-8z-TRGHbOUXI1Xq1',
+  database: 'zcejjhgh',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
