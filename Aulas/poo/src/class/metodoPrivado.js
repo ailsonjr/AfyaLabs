6436b0d3e2da6f1695141455a23cb01729783e57
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-28 23:56:01 -0300

    Adicionado arquivos da aula de Métodos Públicos e Privados

---
+++ Aulas/poo/src/class/metodoPrivado.js
@@ -0,0 +1,42 @@
+class Person {
+  #name;
+  #age;
+  constructor({ name, age }) {
+    this.#name = name;
+    this.#age = age;
+  }
+
+  speak() {
+    return console.log(`Seu nome é ${this.#name} e sua idade é ${this.#age} anos`)
+  }
+
+  set name(string) {
+    this.#name = string;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set age(number) {
+    this.#age = string;
+  }
+
+  get age() {
+    return this.#age;
+  }
+}
+
+class Employee extends Person {
+  #codelanguage
+  constructor({ name, age, codelanguage }) {
+    super({ name, age });
+    this.#codelanguage = codelanguage;
+  }
+
+  stack() {
+    return console.log(`Seu nome é ${this.name}, sua idade ${this.age} e programa em ${this.#codelanguage}`);
+  }
+}
+
+module.exports = Employee;
--- Aulas/poo/src/index.js
+++ Aulas/poo/src/index.js
@@ -2,6 +2,24 @@ const objJavascript = require('./literals/objeto');
 const ObjJSON = require('./literals/objeto.json');
 const Pessoa = require('./class//anatomia');
 const mariane = require('./class/gettersAndSetters');
+const Person = require('./class/metodoPublico');
+const Employee = require('./class/metodoPrivado');
+
+let maria = new Employee({
+  name: 'Maria',
+  age: 19,
+  codelanguage: 'Javascript'
+});
+
+maria.speak();
+maria.stack();
+
+// let marco = new Person({
+//   name: 'Marco',
+//   age: 22
+// })._name;
+
+// console.log(marco)
 
 // console.log(objJavascript.getFinalPrice());
 
