27c12bfccb9dde84ee9b371947a1602f8017e1a9
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-31 12:06:12 -0300

    Implementado programa orientado a objetos para selecionar de maneira aleatória umas das 3 opções do jokenpo

--- Aulas/jokenpo/src/index.js
+++ Aulas/jokenpo/src/index.js
@@ -1,6 +1,13 @@
-const rl = require('./interface');
+// const rl = require('./interface');
 
-rl.question('Você está gostando do POO? ', resposta => {
-  console.log(`Sua resposta foi ${resposta}`);
-  rl.close();
-});
+const MachineOptions = require("./controller/class");
+const options = require("./data");
+
+// rl.question('Você está gostando do POO? ', resposta => {
+//   console.log(`Sua resposta foi ${resposta}`);
+//   rl.close();
+// });
+
+let sorteio = new MachineOptions({ opt: options }).sort();
+
+console.log(sorteio);
