435ed84a411a46b0b640ba143749a7e32bb0577d
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-06-02 17:11:45 -0300

    Feito a resposividade da página

--- Aulas/template-node-fulljs/src/views/components/section/homeSectionOne.js
+++ Aulas/template-node-fulljs/src/views/components/section/homeSectionOne.js
@@ -3,11 +3,12 @@ import backgroundOne from '../../../assets/images/background/background.jpeg';
 
 let HomeSectionOne = `
 <section id="section-one" class="section background--one" style="background-image: url(${backgroundOne})">
-  <div class="container block-section--one">
-    <h1>CLIQUE NI BABY YODA, ELE TE MOSTRARÁ O CAMINHO
-      <h1 />
+  <div>
+    <div class="container block-section--one">
+      <h1>CLIQUE NI BABY YODA, ELE TE MOSTRARÁ O CAMINHO<h1 />
       <img src=${Logo} alt='Logo Star Wars' width=300px />
-  </div>
+    </div>
+  <div/>
 </section>
 `
 
