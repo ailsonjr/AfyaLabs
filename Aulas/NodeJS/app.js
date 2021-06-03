137ef1e6a4f035cb776850a233aa4f933310c0b9
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-06-03 18:28:43 -0300

    Criado uma API em NodeJS para criar, atualizar, selecionar e deletar usuários

--- Aulas/NodeJS/app.js
+++ Aulas/NodeJS/app.js
@@ -2,6 +2,8 @@
 const http = require('http');
 const url = require('url');
 const queryString = require('query-string');
+const fs = require('fs');
+
 
 // Definição de endereço / URL
 const hostname = '127.0.0.1'; // localhost
@@ -9,24 +11,47 @@ const port = 3000;
 
 // Implementeção da regra de negócio
 const server = http.createServer((req, res) => {
+  let resposta;
+  const urlParse = url.parse(req.url, true);
+  // Receber informações do usuário
+  const params = queryString.parse(urlParse.search);
 
-  // Pegar a pergunta na url
-  const params = queryString.parse(url.parse(req.url, true).search);
+  // Criar usuario - atualizar usuario
+  if (urlParse.pathname == '/criar-atualizar-usuario') {
+    // Salvar as informações
+    fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+      if (err) throw err;
+      console.log('Saved');
 
-  // Verificar a pergunta e escolher uma resposta
-  let resposta = '';
-  if (params.pergunta == 'melhor-filme') {
-    resposta = 'Star Wars';
-  } else if (params.pergunta == 'melhor-tecnologia-backend') {
-    resposta = 'NodeJS';
-  } else {
-    resposta = 'Não sei, desculpe :(';
+      resposta = 'Usuario criado com sucesso!'
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+    });
   }
+  // Seleciona usuario
+  else if (urlParse.pathname == '/selecionar-usuario') {
+    fs.readFile('users/' + params.id + '.txt', function (err, data) {
+      resposta = data;
 
-  // Retornar a resposta escolhida
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(resposta);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+    });
+  }
+  // Deleta usuario
+  else if (urlParse.pathname == '/remover-usuario') {
+    fs.unlink('users/' + params.id + '.txt', function (err) {
+      console.log('File deleted');
+
+      resposta = err ? 'Usuario nao encontrado' : 'Usuario removido.';
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+    });
+  }
 });
 
 // Execução
